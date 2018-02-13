0204fbe5bb849f57954b5f4b45f5b0f40774cfe8
Monica Tamaru Jones <mntamaru@gmail.com>
2018-02-13 17:30:02 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  BRAND = []
+  BRANDS = []
   
   def initialize(brand)
     @brand = brand
@@ -11,7 +11,7 @@ class Shoe
   def brand=(brand)
     @brand = brand
 
-    GENRE << brand
+    BRANDS << brand
   end
 
   def cobble
